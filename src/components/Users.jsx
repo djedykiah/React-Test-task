1d636a63d65a5e84f9afcb8bdaaf78d9878e62a3
Anton.f <felseror@gmail.com>
2019-06-06 00:13:38 +0300

    more users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -6,37 +6,57 @@ import User from './User';
 export default class Users extends Component {
   state = {
     users: [],
+    links: {}
   };
 
   componentDidMount() {
-    this.getUsers();
-  }
+    this.getUsers();   
+  } 
+
 
   getUsers = () => {
     fetch(
-      'https://frontend-test-assignment-api.abz.agency/api/v1/users?page=3&count=6',
+      'https://frontend-test-assignment-api.abz.agency/api/v1/users?page=1&count=6',  
     )
       .then(response => response.json())
       .then((response) => {
-        this.setState({ users: response.users });
-      });
-  };
+        this.setState({ users: response.users, links: response.links });
+      }) 
+
+
+  };  
+
+  getNextUsers = () => {
+    fetch(
+      this.state.links.next_url,
+    ) 
+    .then(response => response.json()) 
+    .then((response) => {
+      const newUsers = response.users; 
+        this.setState(({ users }) => {
+          const plusUsers = [
+            ...users,
+            ...newUsers,
+          ] 
+
+          return {
+            users: plusUsers, 
+            links: response.links
+          };
+        });
+    }) 
+  }
+
 
   render() {
 
-    const styles = {
-      p: {
-        textAlign: 'center',
-        marginBottom: 60,
-      },
-      btn: {
-        display: 'block',
-      },
-    };
+
+    const buttonVisibility = this.state.links.next_url === null ? 'hidden' : 'visible';
+
     return (
       <div className="users">
         <h1>Our cheerful users</h1>
-        <p style={styles.p}>Attention! Sorting users by registration date</p>
+        <p className="under-title">Attention! Sorting users by registration date</p>
         <Container>
           <Row>
             {this.state.users.map(function (item, index) {
@@ -47,7 +67,7 @@ export default class Users extends Component {
               );
             })}
           </Row>
-          <button type="button" className="btn" style={styles.btn}>
+          <button type="button" onClick={ this.getNextUsers } className={`btn btn-more ${buttonVisibility}`}>
             Show more
           </button>
         </Container>
